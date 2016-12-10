29c719faa8861cc7340d3b20b5278f9cbf87904a
Marcel Greter <marcel.greter@ocbnet.ch>
2016-12-10 19:43:34 +0100

    Update for LibSass 3.4.0 KITT release

--- perl/settings.cmd
+++ perl/settings.cmd
@@ -7,7 +7,7 @@ set PERLDIR=%drivep%\%ARCH%\perl
 
 REM latest version can be queried via 99-shell:
 REM perl tools\query-version.pl CSS::Sass
-set RELVERSION=3.3.5
+set RELVERSION=3.4.0
 
 if #%drive:~-1%# == #\# set drivep=%drive:~0,-1%
 
