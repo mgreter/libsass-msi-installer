f13f821446954bf05eb2392a433c55d3a5f72381
Marcel Greter <marcel.greter@ocbnet.ch>
2018-01-12 02:53:11 +0100

    Update prepare

--- perl/01-prepare.cmd
+++ perl/01-prepare.cmd
@@ -4,33 +4,13 @@ cd /D %~dp0
 call settings.cmd
 call ..\unxutils.cmd
 
-call cpan Test::Output
-call cpanm -f Test::Output
-call cpan install Test::Output
-call cpan install Getopt::ArgvFile
-call cpan install Module::ScanDeps
-REM 0.10 fails cpanm, cpan works?
-REM or cpanm Test::Mock::Guard@0.09
-call cpan install Test::Mock::Guard
 
-REM 0.10+ fails cpanm, broken pipe?
-REM or cpanm ExtUtils::CppGuess@0.09
-call cpan install ExtUtils::CppGuess
+cd %ARCH%
+git clone https://github.com/mgreter/Filesys-Notify-Simple
+cd Filesys-Notify-Simple
+perl Makefile.PL
+dmake -P4 test
+dmake install
+cd ..\..
 
-echo ================================================================================
-
-REM some had problems with tests?
-REM check future if still needed!
-call cpanm Win32::IPC --notest
-call cpanm Win32::Unicode --notest
-
-echo ================================================================================
-
-call cpanm RSCHUPP/PAR-Packer-1.037.tar.gz --notest
-call cpanm List::MoreUtils --notest
-
-echo ================================================================================
-
-REM these seem to work ok!
-call cpan install Win32::ChangeNotify
-call cpan install Filesys::Notify::Simple
+pause
